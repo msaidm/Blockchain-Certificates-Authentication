54f1b9554ae83ea026f5771d1f788e6121defb8a
mohammed <msaidm@windowslive.com>
2020-03-04 22:57:51 +0200

    first commit

---
+++ navigation/BottomTabNavigator.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import { createStackNavigator } from '@react-navigation/stack'
+
+
+const BottomTab = createBottomTabNavigator();
+const INITIAL_ROUTE_NAME = 'Home';
+
+
+export default function BottomTabNavigator({ navigation, route }) {
+  // Set the header title on the parent stack navigator depending on the
+  // currently active tab. Learn more in the documentation:
+  // https://reactnavigation.org/docs/en/screen-options-resolution.html
+  navigation.setOptions({ headerTitle: null });
+
+  return (
+    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-home" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Links"
+        component={LinksScreen}
+        options={{
+          title: 'Wallet',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-wallet" />,
+        }}
+      />
+      
+    </BottomTab.Navigator>
+    
+  );
+}
+
+// function getHeaderTitle(route) {
+//   const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+
+//   switch (routeName) {
+//     case 'Home':
+//       return 'How to get started';
+//     case 'Links':
+//       return 'Links to learn more';
+//   }
+// }
