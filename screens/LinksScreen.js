15b327b5fc957ddc024ff4873294a50a95a8f125
Monica Maged <monica.maged96@gmail.com>
2020-03-26 04:26:08 +0200

    Working flatlist

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import { Header, Component } from 'react';
-import { StyleSheet, Text, View, FlatList,SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Constants from 'expo-constants';
 
 const Tab = createMaterialTopTabNavigator();
 var walletID = "C44H0ImYvrWRpsBVcCHLfjU53UbPUNQiV";
 var connectionName;
 var connectionsArray = [""];
 var currArraySize = 0;
-
 
 function CredentialsScreen() {
    return (
@@ -27,15 +27,27 @@ function ConnectionsScreen() {
    const [wallets, setWallets] = React.useState([]);
    const [connectionName, setConnectionName] = React.useState("");
    const [arraySize, setArraySize] = React.useState(0);
-   var index=0;
-   const DATA=[];
-  
+   const [DATA, setData] = React.useState([]);
+   var index = 0;
+
 
    function Item({ title }) {
       return (
-        <View style={styles.item}>
-          <Text style={styles.title}>{title}</Text>
-        </View>
+         <View style={styles.item}>
+            <Text style={styles.title}>{title}</Text>
+         </View>
+      );
+   }
+
+   FlatListItemSeparator = () => {
+      return (
+        <View
+          style={{
+            height: 1,
+            width: "100%",
+            backgroundColor: "#000",
+          }}
+        />
       );
     }
 
@@ -62,50 +74,26 @@ function ConnectionsScreen() {
       });
       res.json().then(res => setWallets(res)).then(setConnectionName(wallets[0].name)).then(setArraySize(wallets.length))
       console.log(arraySize);
-//      if (arraySize > currArraySize) {
-//         currArraySize = arraySize;
+      if (arraySize > currArraySize) {
+         currArraySize = arraySize;
          for (let index = 0; index < arraySize; index++) {
-            const obj= {id:index, title:wallets[index].name};
+            const obj = { id: index, title: wallets[index].name };
             DATA.push(obj);
-            //connectionsArray = connectionsArray.concat(wallets[index].name)
             console.log("index " + index)
          }
-//      }
+      }
       console.log(DATA);
-      //console.log(connectionsArray)
    }
-   
+
    return (
       <SafeAreaView style={styles.container}>
          <FlatList
             data={DATA}
             renderItem={({ item }) => <Item title={item.title} />}
             keyExtractor={item => item.id}
+            ItemSeparatorComponent={FlatListItemSeparator}
          />
       </SafeAreaView>
-
-//      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-//         <OptionButton
-//            icon="md-school"
-//            label={connectionName}
-//           onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
-//         />
-
-         /* <OptionButton
-            icon="md-compass"
-            label="Read the React Navigation documentation"
-            onPress={() => WebBrowser.openBrowserAsync('https://reactnavigation.org')}
-         />
-
-         <OptionButton
-            icon="ios-chatboxes"
-            label="Ask a question on the forums"
-            onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
-            isLastOption
-         /> */
-
-     // </ScrollView>
-  // );
    );
 }
 
@@ -136,39 +124,8 @@ function MyTabs() {
 
 
 export default function LinksScreen() {
-
-   // const [wallets, setWallets] = React.useState([]);
-   // const [connectionName, setConnectionName] = React.useState("");
-
-   // const setWalletsAndFetch = (wallets) => {
-   //    setWallets(wallets);
-   //    fetchConnections(wallets);
-   // }
-
-   // React.useEffect(() => {
-   //    (async () => {
-   //       console.log("HELLOOO00");
-   //       fetchConnections();
-   //    })();
-   // }, [setWalletsAndFetch]);
-
-   // async function fetchConnections() {
-
-   //    const res = await fetch('https://api.streetcred.id/custodian/v1/api/' + walletID + '/connections', {
-   //       method: 'GET',
-   //       headers: {
-   //          Authorization: 'Bearer dq6RoZ4gJWss_hRtGC_cyUBv66JwZhUbRRKukMPtv4o',
-   //          XStreetcredSubscriptionKey: '0c1596b315f84ac9a4de6810ef464411',
-   //          Accept: 'application/json',
-   //       },
-   //    });
-   //    res.json().then(res => setWallets(res)).then(setConnectionName(wallets[0].name))
-
-   // }
-
    return (
       <MyTabs />
-
       // <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
       //    <OptionButton
       //       icon="md-school"
@@ -210,9 +167,13 @@ function OptionButton({ icon, label, onPress, isLastOption }) {
 }
 
 const styles = StyleSheet.create({
+   // container: {
+   //    flex: 1,
+   //    backgroundColor: '#fafafa',
+   // },
    container: {
       flex: 1,
-      backgroundColor: '#fafafa',
+      marginTop: Constants.statusBarHeight,
    },
    contentContainer: {
       paddingTop: 15,
@@ -237,14 +198,13 @@ const styles = StyleSheet.create({
       marginTop: 1,
    },
    item: {
-      backgroundColor: '#f9c2ff',
-      padding: 20,
+      backgroundColor: '#ff00ff00',
+      padding: 5,
       marginVertical: 8,
       marginHorizontal: 16,
-    },
-    title: {
-      fontSize: 32,
-    },
+   },
+   title: {
+      fontSize: 22,
+   },
 });
 
- 
