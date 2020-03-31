dabb5505bc0fa9cf7b91d0fc611d19f623eb3747
Monica Maged <monica.maged96@gmail.com>
2020-04-01 01:58:23 +0200

    commit3

--- navigation/QRScanner.js
+++ navigation/QRScanner.js
@@ -90,6 +90,7 @@ export default function QRScanner({ navigation }) {
             getWalletId();
             acceptInvitation(userWalletID, invitationFromURL);
             console.log(connections);
+            navigation.navigate("Root");
           }
         },
         {
