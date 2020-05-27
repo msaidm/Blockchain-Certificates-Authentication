2abc2c11be09ef012c1f8b5db6dce0c3e919293a
MarinaGamal <marina.gamal.elias@hotmail.com>
2020-05-27 19:02:10 +0200

    Fixed Bugs

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,23 +2,16 @@ import * as React from 'react';
 import {Platform, StyleSheet, Text, TouchableOpacity, View, Button, FlatList, SafeAreaView,Image } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 import { Card } from 'react-native-elements';
-import useAsyncEffect from 'use-async-effect';
-import useForceUpdate from 'use-force-update';
-import { LinearGradient } from 'expo-linear-gradient';
-import { Ionicons } from '@expo/vector-icons';
-    
-
-var currArraySize2 = 0;
-//var data = [];
+  
 var connectionDataArray=[];
-const ICON_SIZE = 40;
-
+let details2=[];
+let details = [];
+let detailsOfVer =[];
+let detailsOfVer2 =[];
 
 export default function HomeScreen({ route, navigation }) {
 
- 
   var walletID = "C4GTBBcbBMDGunfKF7ySUCH8fHibB4VLZ";
-  //const [DetailsOfVer,setDetailsOfVer]= React.useState([]);
   const [credentials, setCredentials] = React.useState([]);
   const [Verifications, setVerification] = React.useState([]);
   const [offeredCredentials,setOfferedCredentials] = React.useState([]);
@@ -28,40 +21,32 @@ export default function HomeScreen({ route, navigation }) {
   const [arraySize2, setArraySize2] = React.useState(0); 
   const [arraySizeVer, setArraySizeVer] = React.useState(0); 
   const [connectionDetailsArray, setConnectionDetailsArray] = React.useState([]); 
-  //const [connectionDataArray, setConnectionDataArray] = React.useState([]);
   const [connectionDetailsArraySize, setConnectionDetailsArraySize] = React.useState(0);
   const [count, setCount] = React.useState(false);
   const [offeredCredentialsArraySize,setOfferedCredentialsArraySize] = React.useState(0);
   const [RequestedVerificationsArraySize, setRequestedVerificationsArraySize] = React.useState(0);
- // const obj = { id: connectionDetailsArray.connectionId,credentialId:offeredCredentials[index].credentialId, title: connectionDetailsArray.name, image: connectionDetailsArray.imageUrl }; 
+ 
  useInterval(() => {
-  // Your custom logic here
   try {
     const fetchAllCredentials = async() => {
-    
       const data= await fetchCredentials();
       setCredentials(data);
       const dataVer= await fetchVerifications();
-      console.log(dataVer);
-      console.log("HIIIIIIIII");
+
       setVerification(dataVer);
       setArraySizeVer(dataVer.length);
-       //console.log(data.length+"so?")
+
       setArraySize2(data.length);
       details=await fetchOfferedCredentials(data);
-      let DetailsOfVer= await fetchRequestedVerifications(dataVer);
-      console.log("This is the detailsofver");
-      console.log(DetailsOfVer);
-      //setDetailsOfVer(await fetchRequestedVerifications(dataVer));
-      //console.log(DetailsOfVer);
+      detailsOfVer= await fetchRequestedVerifications(dataVer);
+
        setConnectionDetailsArray(details);
-       setVerificationDetailsArray(DetailsOfVer);
+       setVerificationDetailsArray(detailsOfVer);
        setConnectionDetailsArraySize(Object.keys(details).length);
-       setVerificationDetailsArraySize(Object.keys(DetailsOfVer).length);
-    //fetchOfferedCredentials();
-   //console.log("This should be printed after the fetch")
+       setVerificationDetailsArraySize(Object.keys(detailsOfVer).length);
      } 
    fetchAllCredentials();
+   fillConnectionDataArray();
    removeIssuedCredential();
    removeRequestedVerification();
     
@@ -75,24 +60,7 @@ export default function HomeScreen({ route, navigation }) {
   const savedCallback = React.useRef();
   React.useEffect(() => {
     savedCallback.current = callback;
-    const isFocused = navigation.isFocused();
-    // const fetchAllCredentials = async() => {
-    //   try {
-    //    const data= await fetchCredentials();
-    //    setCredentials(data);
-    //     //console.log(data.length+"so?")
-    //     setArraySize2(data.length);
-    //     details=await fetchOfferedCredentials(data);
-    //     setConnectionDetailsArray(details);
-    //     setConnectionDetailsArraySize(Object.keys(details).length);
-    //  //fetchOfferedCredentials();
-    // console.log("This should be printed after the fetch")
-    //   } catch (error) {
-    //     console.log(error)
-    //   }  
-    
-  
-//   
+    const isFocused = navigation.isFocused();  
   },[callback]);
 
   React.useEffect(() => {
@@ -106,18 +74,9 @@ export default function HomeScreen({ route, navigation }) {
   }, [delay]);
 
 }
-
-  fillConnectionDataArray();
-  //console.log(connectionDataArray);
  
- 
- 
- 
-
   function ItemC({ title, url,credentialId }) { //for credential items
     //console.log("render");
-   
-    
     return (
        <TouchableOpacity
           onPress={() => navigation.navigate('OfferDetails',
@@ -143,9 +102,7 @@ export default function HomeScreen({ route, navigation }) {
   }
 
   function ItemV({ title, url,verificationId,}) { //for verification items
-    //console.log("render");
-   
-    
+    //console.log("render");    
     return (
        <TouchableOpacity
           onPress={() => navigation.navigate('VerificationRequestDetails',
@@ -170,27 +127,16 @@ export default function HomeScreen({ route, navigation }) {
     );
   }
 
- function add(array,object) {
-     array.push(object);
-  return array;
-  }
-
   
   function addConnectionDetails(arr, myID, object) {
-    // const { length } = arr;
-    // const id = length + 1;
-    
     const found = arr.some(el => el.id == myID);
     if (!found) {
-      //console().log("BOOOM NOT found")
-      //console.log(myID);
        arr.push(object);
     }
-    // else {//console().log("BOOOM found")}
     return arr;
  }
- async function fetchCredentials() {
-    
+
+ async function fetchCredentials() {  
   const res = await fetch('https://api.streetcred.id/custodian/v1/api/' + walletID + '/credentials', {
      method: 'GET',
      headers: {
@@ -200,26 +146,16 @@ export default function HomeScreen({ route, navigation }) {
      },
   });
   var cred = await res.json();
-  //setCredentials(cred);
-  
- //console.log(count);
- 
  return cred;   
 }
 
   async function fetchOfferedCredentials(data)
   { 
-    
-    // if(count<=5){
-    //   setCount(count +1);}
-    
       currArraySize2 = arraySize2 ;
       for (let index = 0; index < data.length ; index++) 
       {
         if(data[index].state=="Offered")
-        {
-          //console.log(data[index]+"dah mafrod crediial [index]");
-          
+        {          
           setOfferedCredentials(addConnectionDetails(offeredCredentials,data[index].credentialId ,data[index]));
           let tempConnectionID= data[index].connectionId;
           const res = await fetch('https://api.streetcred.id/custodian/v1/api/'+walletID+'/connections/'+tempConnectionID, {
@@ -230,95 +166,48 @@ export default function HomeScreen({ route, navigation }) {
                 Accept: 'application/json',
             },
           });
+          var connection = await res.json();
+          details2 = addConnectionDetails(details,connection.connectionId,connection)
           setOfferedCredentialsArraySize(offeredCredentials.length);
-         let details= res.json();
-         return details
         }
       }
+      return details2
     
   }
 
   function fillConnectionDataArray()
   {
     try {
-      // console.log(Object.keys(connectionDataArray).length+"size el obbj")
-      //console.log(connectionDataArray)
-      console.log(VerificationDetailsArraySize);
-      if(VerificationDetailsArraySize>0)
-      {
-        
-      //console.log(offeredCredentials.length+"size gowa")
-      for (let index = 0; index < requestedVerifications.length; index++) {
-       // console.log("da gowa el for ");
-      //console.log(connectionDetailsArray.connectionId);
-      if( VerificationDetailsArray.connectionId !== null){
-      console.log("kda da5lt");
-      //setCount(false);
-      const objj = { id: VerificationDetailsArray.verificationId,verificationId:requestedVerifications[index].verificationId, title: VerificationDetailsArray.name, image: VerificationDetailsArray.imageUrl , type:'Verification' }; 
-      connectionDataArray=addConnectionDetails(connectionDataArray,objj.id,objj); 
-      //console.log(obj);
-      }
-  
-      if(connectionDetailsArraySize>0)
-      {
-        //console.log("kda da5lt")
-      //console.log(offeredCredentials.length+"size gowa")
-      for (let index = 0; index < offeredCredentials.length; index++) {
-       // console.log("da gowa el for ");
-      //console.log(connectionDetailsArray.connectionId);
-      if( connectionDetailsArray.connectionId !== null){
-      const obj = { id: connectionDetailsArray.connectionId,credentialId:offeredCredentials[index].credentialId, title: connectionDetailsArray.name, image: connectionDetailsArray.imageUrl , type:'Credential'}; 
-      connectionDataArray=addConnectionDetails(connectionDataArray,obj.id,obj); 
-      //console.log(obj);
+        if(VerificationDetailsArraySize>0)
+        {
+          for (let index = 0; index < requestedVerifications.length; index++) {
+            for (let index2 = 0; index2 < VerificationDetailsArray.length; index2++) {
+              if( VerificationDetailsArray[index2].connectionId === requestedVerifications[index].connectionId){
+              const objj = { id: VerificationDetailsArray[index2].connectionId,verificationId:requestedVerifications[index].verificationId, title: VerificationDetailsArray[index2].name, image: VerificationDetailsArray[index2].imageUrl , type:'Verification' }; 
+              connectionDataArray=addConnectionDetails(connectionDataArray,objj.id,objj); 
+            }
+          }
+          if(connectionDetailsArraySize>0)
+          {
+            for (let index = 0; index < offeredCredentials.length; index++) {
+              for (let index2 = 0; index2 < connectionDetailsArray.length; index2++) {
+                if( connectionDetailsArray[index2].connectionId === offeredCredentials[index].connectionId){
+                  const obj = { id: connectionDetailsArray[index2].connectionId,credentialId:offeredCredentials[index].credentialId, title: connectionDetailsArray[index2].name, image: connectionDetailsArray[index2].imageUrl, type:'Credential' };  
+                  connectionDataArray=addConnectionDetails(connectionDataArray,obj.id,obj); 
+                }
+              }
+            }
+          }
+        }
+        console.log(connectionDataArray);
       }
       }
-      
-    }
-  }
-  console.log(connectionDataArray);
-}
-}
-  catch (error) {
-      console.log(error)
-    }
-
-      
+    catch (error) {
+        console.log(error)
+    }   
   }
     
-  // }
-  //         //console.log(offeredCredentials);
-  //       // console.log(connectionDetailsArray);
-  //         console.log(connectionDetailsArray.connectionId);
-  //         const obj = { id: connectionDetailsArray.connectionId,credentialId:offeredCredentials[index].credentialId, title: connectionDetailsArray.name, image: connectionDetailsArray.imageUrl };    
-  //         setConnectionDataArray(addConnectionDetails(connectionDataArray,obj.id,obj)); 
-  //         console.log("connectionDataArray");
-  //         console.log(connectionDataArray);
-  //             }
-  //     }  
-  //   }
-    
-  //   for (let index = 0; index < offeredCredentials.length; index++)
-  //   {   
-      
-     
-  //   }
-  
-    
-  //   // if(count<50)
-  //   // setOfferedCredentialsArraySize(count+1);
-  //   // if(currArraySize2!=arraySize2)
-  //   // setCount(count+1);
-  //   console.log(offeredCredentials.length+"deh?"); 
-
-  // }
-//   const innerFunction = useCallback(() => {
-//     // do something!
-// });
-
- 
-  //fetchOfferedCredentials(data);
   async function fetchVerifications() {
-    
     const res = await fetch('https://api.streetcred.id/custodian/v1/api/' + walletID + '/verifications', {
        method: 'GET',
        headers: {
@@ -328,27 +217,16 @@ export default function HomeScreen({ route, navigation }) {
        },
     });
     var ver = await res.json();
-    console.log(res.json());
-    console.log("betba3y tayeb???");
-            //setCredentials(cred);
-    
-   //console.log(count);
-   
    return ver;   
   }
+
   async function fetchRequestedVerifications(dataVer)
   { 
-    
-    // if(count<=5){
-    //   setCount(count +1);}
-    
       currArraySizeVer= arraySizeVer ;
       for (let index = 0; index < dataVer.length ; index++) 
       {
         if(dataVer[index].state=="Requested")
         {
-          //console.log(data[index]+"dah mafrod crediial [index]");
-          
           setrequestedVerifications(addConnectionDetails(requestedVerifications,dataVer[index].verificationId ,dataVer[index]));
           let tempVerConnectionID= dataVer[index].connectionId;
           const res = await fetch('https://api.streetcred.id/custodian/v1/api/'+walletID+'/connections/'+tempVerConnectionID, {
@@ -359,13 +237,14 @@ export default function HomeScreen({ route, navigation }) {
                 Accept: 'application/json',
             },
           });
+          
+          var ver = await res.json();
+          detailsOfVer2 = addConnectionDetails(detailsOfVer,ver.connectionId,ver)
           setOfferedCredentialsArraySize(offeredCredentials.length);
           setRequestedVerificationsArraySize(requestedVerifications.length);
-         let DetailsOfVer= res.json();
-         return DetailsOfVer ;
         }
       }
-    
+      return detailsOfVer2;
   }
   
   function removeIssuedCredential(){
@@ -378,23 +257,19 @@ export default function HomeScreen({ route, navigation }) {
           for (let index2 = 0; index2 < connectionDataArray.length; index2++) {
               if(connectionDataArray[index2].credentialId == credentials[index].credentialId)
               {
-                //console.log("here")
                 connectionDataArray.splice(index2,1)
                 setOfferedCredentialsArraySize(offeredCredentials.length);
               }
-          }
-          
+          }    
         }
     }
-    if(offeredCredentialsArraySize>0){
+    if(connectionDataArray.length>0){
       setCount(true)
     }
     else
       setCount(false)  
-
-    //console.log(count)
-    //console.log(offeredCredentialsArraySize)
   }
+
   function removeRequestedVerification(){
     var tempArray=[]; 
     setrequestedVerifications(tempArray);
@@ -405,12 +280,10 @@ export default function HomeScreen({ route, navigation }) {
           for (let index3 = 0; index3 < connectionDataArray.length; index3++) {
               if(connectionDataArray[index3].verificationId == Verifications[index].verificationId)
               {
-                //console.log("here")
                 connectionDataArray.splice(index2,1)
                 setrequestedVerifications(requestedVerifications.length);
               }
-          }
-          
+          }    
         }
     }
     if(connectionDataArray.length>0){
@@ -418,10 +291,8 @@ export default function HomeScreen({ route, navigation }) {
     }
     else
       setCount(false)  
-  
-    //console.log(count)
-    //console.log(offeredCredentialsArraySize)
-  }
+    }
+
 
   return (
     <View style={styles.container}>    
@@ -431,9 +302,6 @@ export default function HomeScreen({ route, navigation }) {
             <FlatList
                 data={connectionDataArray}
                 keyExtractor={item => item.id}
-                //extraData={offeredCredentialsArraySize}
-                //initialNumToRender={1}
-                
                 renderItem={({ item }) => { 
                 if (item.type =="Credential")
                   { 
@@ -467,7 +335,7 @@ export default function HomeScreen({ route, navigation }) {
        
     </View>
   );
-}
+  }
 HomeScreen.navigationOptions = {
   header: null,
   
