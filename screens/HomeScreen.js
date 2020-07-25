d95cf8ab564e1a5f5f91d046eb966f25452d2e05
MaryAbdou <maryselim97@yahoo.com>
2020-07-25 13:27:08 +0200

    home screen modofied for ver

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -72,6 +72,7 @@ export default function HomeScreen({ route, navigation }) {
       } 
     fetchAllCredentials();
     fillConnectionDataArray();
+    console.log(dataVer);
     //fillAttReqArray();
     removeIssuedCredential();
     removeRequestedVerification();
