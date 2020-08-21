442bae5c4a0bfaf5441a67f330aa652d752b21c0
Monica Maged <monica.maged96@gmail.com>
2020-08-22 00:54:44 +0200

    fixed syntax errors

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -183,50 +183,52 @@ export default function DetailsScreen({ route, navigation }) {
       throw err;
     }
   }
+  console.log("ana fe det" + masterDegree)
+
+  return (
+
+    <View style={styles.container}>
+      {masterDegree ?
+
+        (
+          <View style={styles.container}>
+            <Text style={styles.title}>{name}</Text>
+            <Image source={{ uri: image }} style={styles.image} />
+            <Card title="Masters Degree ">
+              <Text style={styles.paragraph}>Student name: {JSON.stringify(Item.sname)}</Text>
+              <Text style={styles.paragraph}>Cummilative GPA: {JSON.stringify(Item.sgpa)}</Text>
+              <Text style={styles.paragraph}>Graduation Year: {JSON.stringify(Item.syear)}</Text>
+              <Text style={styles.paragraph}>Master Degree GPA: 3.85</Text>
+              <Text style={styles.paragraph}>Master Degree Year: 2020</Text>
+            </Card>
+          </View>
+
+        )
+
+        :
+
+        (
+          <View style={styles.container}>
+
+            <Text style={styles.title}>{name}</Text>
+            <Image source={{ uri: image }} style={styles.image} />
+            <Card title={JSON.stringify(Item.type)} >
+              <Text style={styles.paragraph}>Student name: {JSON.stringify(Item.sname)}</Text>
+              <Text style={styles.paragraph}>Cummilative GPA: {JSON.stringify(Item.sgpa)}</Text>
+              <Text style={styles.paragraph}>Graduation Year: {JSON.stringify(Item.syear)}</Text>
+              <Button
+                title="Download PDF" type="outline" onPress={() => createAndSavePDF(htmlContent)} />
+            </Card>
+          </View>
+        )
+      }
+    </View>
+
+  );
+
 };
 
-console.log("ana fe det" + masterDegree)
 
-return (
-
-  <View style={styles.container}>
-    {masterDegree ?
-
-      (
-        <View style={styles.container}>
-          <Text style={styles.title}>{name}</Text>
-          <Image source={{ uri: image }} style={styles.image} />
-          <Card title="Masters Degree ">
-            <Text style={styles.paragraph}>Student name: {JSON.stringify(Item.sname)}</Text>
-            <Text style={styles.paragraph}>Cummilative GPA: {JSON.stringify(Item.sgpa)}</Text>
-            <Text style={styles.paragraph}>Graduation Year: {JSON.stringify(Item.syear)}</Text>
-            <Text style={styles.paragraph}>Master Degree GPA: 3.85</Text>
-            <Text style={styles.paragraph}>Master Degree Year: 2020</Text>
-          </Card>
-        </View>
-
-      )
-
-      :
-
-      (
-        <View style={styles.container}>
-
-          <Text style={styles.title}>{name}</Text>
-          <Image source={{ uri: image }} style={styles.image} />
-          <Card title={JSON.stringify(Item.type)} >
-            <Text style={styles.paragraph}>Student name: {JSON.stringify(Item.sname)}</Text>
-            <Text style={styles.paragraph}>Cummilative GPA: {JSON.stringify(Item.sgpa)}</Text>
-            <Text style={styles.paragraph}>Graduation Year: {JSON.stringify(Item.syear)}</Text>
-            <Button
-              title="Download PDF" type="outline" onPress={() => createAndSavePDF(htmlContent)} />
-          </Card>
-        </View>
-      )
-    }
-  </View>
-
-);
 
 
 
